d2e4c31027717fdfde4792d00096303241a0f8cc
jtaylor024 <jrt99@byu.edu>
2025-12-03 16:46:53 -0700

    Updated data_generation to be more general and outputs a csv to be run by dataAssociation

--- JeffreysCode/dataAssociation.py
+++ JeffreysCode/dataAssociation.py
@@ -29,33 +29,50 @@ def updatelandmark(mu, Sigma, z, R):
     Sigma_new = (np.eye(len(mu)) - K) @ Sigma
     return mu_new, Sigma_new
 
-def drawline(point1, point2):
-    x_values = [point1[0], point2[0]]
-    y_values = [point1[1], point2[1]]
-    plt.plot(x_values, y_values, color='gray', linewidth=0.3, label ='association')
 
-def plotpoints(allpoints, mu):
+def plotpoints(allpoints, mu, plotLine = True):
+    ''' Plots all estimates, the landmarks, and the associations between them '''
     frequency = np.zeros(len(mu))
+    # plot all estimate points
     for set in allpoints:
         point = set[0]
         idx = set[1]
         frequency[idx] += 1
         plt.scatter(point[0], point[1], color='red', s=1, label = 'photo estimate')
-        drawline(point, mu[idx])
+    # plot association line
+        if plotLine:
+            x_values = [point[0], mu[idx][0]]
+            y_values = [point[1], mu[idx][1]]
+            plt.plot(x_values, y_values, color='gray', linewidth=0.3, label ='association')
+
+    # plot landmarks
     for i, point in enumerate(mu):
-        if frequency[i] > 1:
-            plt.scatter(point[0], point[1], color='blue', s=2, label = 'landmark')
-        else:
-            plt.scatter(point[0], point[1], color='white', s=2, label = 'landmark')
+        col = 'blue' if frequency[i] > 1 else 'white'
+        plt.scatter(point[0], point[1], color=col, s=2, label = 'landmark')
+    
+    # legend targets (proxy artists)
+    photo_proxy = plt.Line2D([0], [0], marker='o', color='red', linestyle='None', markersize=2, label='photo estimate')
+    landmark_proxy = plt.Line2D([0], [0], marker='o', color='blue', linestyle='None', markersize=3, label='landmark')
+    assoc_proxy = plt.Line2D([0], [0], color='gray', linewidth=0.4, label='association')
+    plt.legend(handles=[photo_proxy, landmark_proxy, assoc_proxy], loc='upper left')
+    plt.axis('equal')
 
 def associate_landmarks(df):
     mu = []
     Sigma = []
     allpoints = []
-    df['GPSparsed'] = df['GPSpoint'].apply(convertGPS)
+    try:
+        df['GPSparsed'] = df['GPSpoint'].apply(convertGPS)
+    except:
+        pass
     for index, row in df.iterrows():
-        for point in row['GPSparsed']:
-            point = GPS2meters(point)
+        # for point in row['GPSparsed']:
+            point = row['GPSparsed']
+            # try:
+            #     point = GPS2meters(point)
+            #     print('converted point to meters')
+            # except:
+            #     pass
             unc = row['horizontal_accuracy']
             Sigma_bar = np.eye(2) * unc
             association = associateData(point, mu, Sigma_bar)
@@ -73,29 +90,25 @@ def associate_landmarks(df):
 if __name__ == "__main__":
     start = time.time()
 
-    csvpath  = "subtest.csv" # define csv 
+    csvpath  = "simulated_flower_data.csv" # define csv 
+
     
     # create df from csv
     df = pd.read_csv(csvpath)
     # df = df[df['flower?'] == 1]
     # df = df[df['sourceFolder'] == '10-31_2pm']
     # df = df[df['latitude'].notnull() & df['longitude'].notnull()]
-    # df = df.iloc[:1000]
+    df = df.iloc[:1000]
+    df['GPSparsed'] = df['GPSparsed'].apply(lambda x: np.fromstring(x.strip('[]'), sep=' '))
+    print(df.head())
 
     print('time to load new csv is ', time.time() - start, 'seconds')
 
     mu, allpoints = associate_landmarks(df)
 
-    print('Number of landmarks: ', len(mu))
-
+    print('Number of detected landmarks: ', len(mu))
     plotpoints(allpoints, mu)
 
-    # legend targets (proxy artists)
-    photo_proxy = plt.Line2D([0], [0], marker='o', color='red', linestyle='None', markersize=2, label='photo estimate')
-    landmark_proxy = plt.Line2D([0], [0], marker='o', color='blue', linestyle='None', markersize=3, label='landmark')
-    assoc_proxy = plt.Line2D([0], [0], color='gray', linewidth=0.4, label='association')
-    plt.legend(handles=[photo_proxy, landmark_proxy, assoc_proxy], loc='upper left')
-    plt.axis('equal')
     plt.show()
 
 
--- isaacscode/data_generation.py
+++ isaacscode/data_generation.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def generate_flower_data(rows = 2, per_row = 4, density = 0.9, dist=0.1524, sigma=(0.014, 0.025)): # 6in <--> 0.1524 m   9in <--> 0.2286 m  12in <--> 0.3048 m
+    # np.random.seed(42)  # make random draws reproducible
+    map = []  # placeholder for flower map
+    for i in range(per_row):
+        for j in range(rows):
+            if np.random.rand() < density:
+                x = np.random.normal(loc=j * -dist, scale=np.random.uniform(*sigma))
+                y = np.random.normal(loc=i * dist, scale=np.random.uniform(*sigma))
+                map.append([x, y])
+                # plt.plot(map[i,j][0], map[i,j][1], 'kx')  # plot flower location
+    # plt.show()
+    return map
 
 
-
-def generate_flower_data(dist=6, sigma=(1.4, 2.5)):
+def generate_measurement_data(map, sigma=(0.014, 0.025)):
 
     # make random draws reproducible
     np.random.seed(42)
@@ -13,37 +26,52 @@ def generate_flower_data(dist=6, sigma=(1.4, 2.5)):
     sigmas = []
     known_landmarks = []
 
-    for i in range(5):
-        mean = np.array([0.0, dist * (i + 1)])
+    #    Iterate through each landmark in the map
+    for i, landmark in enumerate(map):
+        mean = landmark
         sigma_sample = np.random.uniform(*sigma)
-        samples = np.random.normal(loc=mean, scale=sigma_sample/2.54, size=(10, 2))
+        samples = np.random.normal(loc=mean, scale=sigma_sample, size=(10, 2))
         for s in samples:
             points.append(s)
             sigmas.append(sigma_sample)
             known_landmarks.append(i+1)
 
-
+        
     return points, sigmas, known_landmarks
 
+def write_csv(points, sigmas, filename='simulated_flower_data.csv'):
+
+    data = {
+        'GPSparsed': points,
+        'horizontal_accuracy': sigmas
+    }
+
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
 
-    dist = 6
-    sigma = (1.4, 2.5)
+    dist = 0.1524  # distance between flowers in meters (6 inches)
+    sigma = (0.014, 0.025) # range of variances
 
-    points, sigmas, known_landmarks = generate_flower_data(dist, sigma)
-
-    colors = {1: 'red', 2: 'orange', 3: 'green', 4: 'blue', 5: 'purple'}
-
-    plt.figure(figsize=(8, 8))
+    map = generate_flower_data(dist=dist, sigma=sigma)
+    print(map)
+    points, sigmas, known_landmarks = generate_measurement_data(map, sigma)
+    # colors = {1: 'red', 2: 'orange', 3: 'green', 4: 'blue', 5: 'purple'}
+    N = len(map) + 1
+    cmap = plt.cm.tab20.colors  # gives a list of 20 colors
+    colors = [cmap[i % 20] for i in range(N)]
+    # plt.figure(figsize=(8, 8))
+    plt.axis('equal')
     for i, point in enumerate(points):
-        plt.scatter(point[0], point[1], s=20, color=colors[known_landmarks[i]], alpha=0.6)
-    for i in range(5):
-        plt.scatter(0, (i+1)*dist, s=20, color=colors[i+1], marker='X', label=f'Landmark {i+1}')
+        plt.scatter(point[0], point[1], s=10, color=colors[known_landmarks[i]], alpha=0.7, edgecolors=None)
+    for i, landmark in enumerate(map):
+        plt.scatter(landmark[0], landmark[1], s=10, color=colors[i+1], marker='X', edgecolors='k', label=f'Landmark {i+1}')
     plt.title('Simulated Flower Data with Gaussian Noise')
-    plt.xlabel('X Coordinate (meters)')
-    plt.ylabel('Y Coordinate (meters)')
-    plt.grid()
+    plt.xlabel('N/S [m]')
+    plt.ylabel('E/W [m]]')
+    # plt.grid()
     # plt.legend()
     plt.axis('equal')
+    write_csv(points, sigmas)
     plt.show()
